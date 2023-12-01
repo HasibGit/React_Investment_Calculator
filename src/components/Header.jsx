f30b387ef4041873dc6f66084fd4b718ea2c65a6
HasibGit <hasibullah04@gmail.com>
2023-12-01 12:56:02 +0600

    header section design done

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div id="header">
+      <img
+        src="src/assets/investment-calculator-logo.png"
+        alt="investment calculator"
+      />
+      <h1>Investment Calculator</h1>
+    </div>
+  );
+}
