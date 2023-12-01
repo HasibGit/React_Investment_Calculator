036326cf51cf8f9c36aa42f6e9ad9e40d635500a
HasibGit <hasibullah04@gmail.com>
2023-12-01 13:09:42 +0600

    user input section design done

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,28 @@
+export default function UserInput() {
+  return (
+    <div id="user-input-container">
+      <div className="input-group">
+        <div id="user-input">
+          <label>Initial Investment</label>
+          <input type="number" id="user-input" />
+        </div>
+
+        <div id="user-input">
+          <label>Initial Investment</label>
+          <input type="number" id="user-input" />
+        </div>
+      </div>
+
+      <div className="input-group">
+        <div id="user-input">
+          <label>Initial Investment</label>
+          <input type="number" id="user-input" />
+        </div>
+        <div id="user-input">
+          <label>Initial Investment</label>
+          <input type="number" id="user-input" />
+        </div>
+      </div>
+    </div>
+  );
+}
